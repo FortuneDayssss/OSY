dd58228863f2fec1b73f9effa9be34b48206b4f5
fortunedayssss <717622995@qq.com>
2017-06-23 09:00:34 +0000

    elf loader

--- prog/boot/setup.asm
+++ prog/boot/setup.asm
@@ -1,4 +1,5 @@
 %include "macro.inc"
+KERNEL_ELF_ADDR equ     00050000h
 org 00090000h
     jmp     SETUP16_ENTRY
 
@@ -97,15 +98,126 @@ STACK32_TOP equ     $ - 1
 [SECTION .setup32]
 [BITS 32]
 SETUP32_ENTRY:
+    ;init segment register for protect mode
     mov     ax,     SELECTOR_MEMD
     mov     ds,     ax
     mov     es,     ax
     mov     ss,     ax
     mov     esp,    STACK32_TOP
+    mov     ebp,    esp
     mov     ax,     SELECTOR_VIDEO
     mov     gs,     ax
 
-    ;page
+    ;load kernel code from elf to correct addr
+    mov     ebx,    KERNEL_ELF_ADDR
+    add     ebx,    42
+    xor     eax,    eax
+    mov     ax,     [ds : ebx]              ;elfHeader + 42 = elfHeader->e_phentsize(2Byte)
+    push    eax                             ;programHeaderSize [ss : ebp - 4]
+
+    mov	    ebx,    KERNEL_ELF_ADDR
+    add	    ebx,    28
+    mov	    eax,    [ds : ebx]              ;elfHeader + 28 = elfHeader->e_phoff(4Byte)
+    mov	    ebx,    KERNEL_ELF_ADDR
+    add	    ebx,    eax
+    push    ebx                             ;programHeaderAddress [ss : ebp - 8]
+
+    mov     ebx,    KERNEL_ELF_ADDR
+    add     ebx,    46
+    xor     eax,    eax
+    mov     ax,     [ds : ebx]              ;elfHeader + 46 = elfHeader->e_shentsize(2Byte)
+    push    eax                             ;sectionHeaderSize [ss : ebp - 12]
+
+    mov		ebx,    KERNEL_ELF_ADDR
+    add		ebx,    32
+    mov		eax,    [ds : ebx]              ;elfHeader + 32 = elfHeader->e_shoff(4Byte)
+    mov		ebx,    KERNEL_ELF_ADDR
+    add		ebx,    eax
+    push    ebx                             ;sectionHeaderAddress [ss : ebp - 16]
+
+    mov     ebx,    KERNEL_ELF_ADDR
+    add     ebx,    44
+    xor     ecx,    ecx
+    mov     cx,     [ds : ebx]              ;elfHeader + 44 = elfHeader->e_phnum(2Byte)
+    dec     cx                              ;???last program header always size == 0???
+
+;---------------------------------------------------------------------------------------------
+Load_Kernel_Program_Loop:
+    mov     ebx,    [ebp - 8]               ;ebx <= &(first program header)
+    mov     eax,    [ds : ebx + 20]         ;prgmHeader + 20 = prgmHeader->p_memsz(4Byte)
+    push    eax                             ;size
+
+    mov     eax,    [ds : ebx + 8]          ;prgmHeader + 8 = prgmHeader->p_vaddr(4Byte)
+    push    eax                             ;opAddress
+
+    mov     eax,    [ds : ebx + 4]
+    mov     ebx,    KERNEL_ELF_ADDR
+    add     ebx,    eax
+    push    ebx                             ;rsrcAddress
+
+    call    ASM_Memcpy
+    add     esp,    12
+
+	;prgmHeader = prgmHeader + prgmHeaderSize(= nextPrgmHeader)
+    mov     ebx,    [ebp - 8]
+    mov     eax,    [ebp - 4];/////////////////////////////////////////////////
+    add     ebx,    eax
+    mov     [ebp - 8],  ebx
+	
+	loop    Load_Kernel_Program_Loop
+
+    mov     ebx,    KERNEL_ELF_ADDR
+    add     ebx,    48
+    xor     ecx,    ecx
+    mov     cx,     [ds : ebx]              ;elfHeader + 44 = elfHeader->e_shnum(2Byte)
+Load_Kernel_Section_Loop:
+	mov		ebx,		[ebp - 16];ebx <= &(first section header)
+
+	;if(sectionHeader->sh_type == SHT_NOBIT(00000008))memset0(section, sectionLength);	
+    mov     eax,    [ds : ebx + 4]          ;sectionHeader + 4 = sectionHeader->sh_type(4Byte)
+	cmp     eax,    00000008h
+	jnz     ISNT_NOBITS
+	mov     eax,    [ds : ebx + 20]
+	push    eax
+	mov     eax,    [ds : ebx + 12]
+	push    eax
+	call    ASM_Memset0
+	add     esp,    8
+	jmp     Load_Kernel_Section_Continue
+
+ISNT_NOBITS:
+    ;if(sectionHeader->sh_addr == 0)continue;
+    mov     eax,    [ds : ebx + 12]
+    cmp     eax,    0
+    jz      Load_Kernel_Section_Continue
+
+    mov     eax,    [ds : ebx + 20]         ;sectHeader + 20 = sectHeader->sh_size(4Byte)
+    push    eax;size
+
+    mov     eax,    [ds : ebx + 12]         ;sectHeader + 12 = sectHeader->sh_addr(4Byte)
+	push    eax;opAddress
+
+    mov     eax,    [ds : ebx + 16]
+    mov     ebx,    KERNEL_ELF_ADDR
+    add     ebx,    eax
+    push    ebx                             ;rsrcAddress
+
+    call    ASM_Memcpy
+    add     esp,    12
+
+Load_Kernel_Section_Continue:
+
+    ;sectHeader = sectHeader + sectHeaderSize(= nextSectHeader)
+    mov     ebx,    [ebp - 16]
+    mov     eax,    [ebp - 12];//////////////////////////////////////
+    add     ebx,    eax
+    mov     [ebp - 16], ebx
+	
+    loop    Load_Kernel_Section_Loop
+
+;---------------------------------------------------------------------------------------------
+
+    ;paging
     mov     ax,     SELECTOR_PDIR
     mov     es,     ax
     xor     ebx,    ebx
@@ -138,16 +250,64 @@ INIT_PAGE_TBL_LOOP:
     jmp     PAGING_OK
 PAGING_OK:
 
+;test: jump to kernel.asm
+    jmp     00010000h
+
 TEST_LOOP:
     jmp     TEST_LOOP
 
 
 
 
+;code from OSTOY
+;-------------------function--------------------------------------------
+;void ASM_Memcpy(byte4 rsrcAddr, byte4 opAddr, byte4 size)
+;ds:rsrcAddr => es:opAddr
+ASM_Memcpy:
+	push		ebp
+	mov		ebp,		esp
+	push		eax
+	push		ecx
+	push		esi
+	push		edi
 
+	mov		esi,		[ebp + 8]	;rsrcAddr
+	mov		edi,		[ebp + 12]	;opAddr
+	mov		ecx,		[ebp + 16]	;size
+	shr		ecx,		1
+	add		ecx,		1
+MEMCOPY_LOOP:
+	mov		eax,		[ds : esi]
+	mov		[ds : edi],	eax
+	add		esi,		4
+	add		edi,		4		
+	loop		MEMCOPY_LOOP
 
+	pop		edi
+	pop		esi
+	pop		ecx
+	pop		eax
+	pop		ebp
+	ret
+;void memset0(u32 opAddr, u32 length)
+ASM_Memset0:
+	push		ebp
+	mov		ebp,		esp
+	push		eax
+	push		ecx
+	push		esi
 
+	mov		esi,		[ebp + 8]	;opAddr
+	mov		ecx,		[ebp + 12]	;length
+	add		ecx,		1
+	mov		al,		0
+Memset_LOOP:
+	mov		[ds : esi],	al
+	add		esi,		1		
+	loop		Memset_LOOP
 
-
-
-;test
+	pop		esi
+	pop		ecx
+	pop		eax
+	pop		ebp
+	ret
